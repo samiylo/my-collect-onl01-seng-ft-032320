7e23dd346e4954f0b7946e31a76309ef9cbec967
Samiylo Kryshu <skryshu@gmail.com>
2020-03-25 11:58:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
 
 
+
+
+def my_collect(arg)
+  if block_given?
+    counter = 0 
+    modified_collection = []
+    
+    while counter < arg.length 
+      yield arg[counter]
+      print arg[counter]
+      modified_collection << arg[counter]
+      counter += 1 
+    end
+    modified_collection
+  else
+    puts "No block was given."
+  end
+end
+
